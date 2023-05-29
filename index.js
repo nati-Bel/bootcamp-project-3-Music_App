63b4299e354e7b02233a5bb80652b93cd3be0f49
anaencinasd <ana.encinasdiaz>
2023-05-29 15:04:29 +0200

    batería con animaciones y sonido

--- index.js
+++ index.js
@@ -28,3 +28,41 @@ highTomPlay.addEventListener ("click", () => {
     audioTag.setAttribute("src", "sonidos/tom1.wav")
     audioTag.play()
 })
+
+
+
+
+let rideCymbalPlay = document.getElementById("rideCymbal")
+
+rideCymbalPlay.addEventListener ("click", () => {
+    let audioTag = document.createElement("audio")
+    audioTag.setAttribute("src", "sonidos/redoblante.wav")
+    audioTag.play()
+})
+
+let floorTomPlay = document.getElementById ("floorTom")
+
+floorTomPlay.addEventListener ("click", () =>{
+    let audioTag = document.createElement("audio")
+    audioTag.setAttribute("src", "sonidos/tom4.wav")
+    audioTag.play()
+
+})
+
+let midTomPlay = document.getElementById ("midTom")
+
+midTomPlay.addEventListener ("click", () =>{
+    let audioTag = document.createElement("audio")
+    audioTag.setAttribute("src", "sonidos/tom2.wav")
+    audioTag.play()
+})
+
+let snareDrumPlay = document.getElementById ("snareDrum")
+
+snareDrumPlay.addEventListener ("click", () => {
+
+    let audioTag = document.createElement("audio")
+    audioTag.setAttribute("src", "sonidos/tom3.wav")
+    audioTag.play()
+
+})
